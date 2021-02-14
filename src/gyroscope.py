e4346a10d1806848793151b9fead2880fac38b54
Ben Nason <nasonb@wellingtoncollege.org.uk>
2021-02-14 17:58:36 +0000

    Gyroscope - corrected Data repeating problem to test in emulator just get rid of the class. If i have got it wrong, tell me. still haven't implemented moving window average, but it will be easier now

--- src/gyroscope.py
+++ src/gyroscope.py
@@ -1,29 +1,17 @@
 from sense_hat import SenseHat
-gyroList = [{
+
+gyroOrent ={
   'pitch': 76.2714433283,
   'roll': 237.873941872,
   'yaw': 71.2012831036
-}, {
-  'pitch': 76.2714433283,
-  'roll': 237.873941872,
-  'yaw': 71.2012831036
-}, {
-  'pitch': 76.2714433283,
-  'roll': 237.873941872,
-  'yaw': 71.2012831036
-}, {
-  'pitch': 76.2714433283,
-  'roll': 237.873941872,
-  'yaw': 71.2012831036
-}]
+} # test data
 
 
-def moving_window_average(x, n_neighbors=3):
-  n = len(x)
-  width = n_neighbors * 2 + 1
-  x = [x[0]] * n_neighbors + x + [x[-1]] * n_neighbors
-  # To complete the function,
-  return [sum(x[i:i + width]) * 2 / n for i in range(n)]
+# def moving_window_average(x, n_neighbors=3):
+#   n = len(x)
+#   width = n_neighbors * 2 + 1
+#   x = [x[0]] * n_neighbors + x + [x[-1]] * n_neighbors
+#   return [sum(x[i:i + width]) * 2 / n for i in range(n)]
 
 
 class GyroscopeController:
@@ -40,15 +28,11 @@ class GyroscopeController:
   yawList = []
 
 
-while True:
-  gyro_only = sense.get_gyroscope()
-  gyroList.append(sense.get_gyroscope())
-  for i in gyroList:
-    pitchList.append([i[name] for name in i.keys() if name == 'pitch'])
-    rollList.append([i[name] for name in i.keys() if name == 'roll'])
-    yawList.append([i[name] for name in i.keys() if name == 'yaw'])
-
-#   print("p: {pitch}, r: {roll}, y: {yaw}".format(**gyro_only))
+  while True:
+    gyroOrent = sense.get_gyroscope()
+    pitchList.append([gyroOrent[i] for i in gyroOrent if i == 'pitch']) 
+    rollList.append([gyroOrent[i] for i in gyroOrent if i == 'roll']) 
+    yawList.append([gyroOrent[i] for i in gyroOrent if i == 'yaw'])
 
 # def __init__(self, con):
 #   self.con = con
