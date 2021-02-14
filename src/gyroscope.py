441d4e39e56b816be2d462feeb290ee4599dcc6d
Ben Nason <nasonb@wellingtoncollege.org.uk>
2021-02-14 17:13:11 +0000

    Gyroscope - This now recieves the Pitch, Roll and yaw then appends them to a list and then i seperate them into individual list, i haven't yet added the moving window average to be functional for this but it is there

--- src/gyroscope.py
+++ src/gyroscope.py
@@ -1,3 +1,13 @@
+from sense_hat import SenseHat
+gyroList = [{'pitch': 76.2714433283, 'roll': 237.873941872, 'yaw': 71.2012831036}, {'pitch': 76.2714433283, 'roll': 237.873941872, 'yaw': 71.2012831036}, {'pitch': 76.2714433283, 'roll': 237.873941872, 'yaw': 71.2012831036}, {'pitch': 76.2714433283, 'roll': 237.873941872, 'yaw': 71.2012831036}]
+
+def moving_window_average(x, n_neighbors=3):
+  n = len(x)
+  width = n_neighbors*2 + 1
+  x = [x[0]]*n_neighbors + x + [x[-1]]*n_neighbors
+  # To complete the function,
+  return [sum(x[i:i+width] )*2/n  for i in range(n) ]
+
 class GyroscopeController:
   """
     Controller for methods and data related to the gyroscope
@@ -5,5 +15,26 @@ class GyroscopeController:
     :param con: Reference to main controller
     :type con: main.Controller
   """
-  def __init__(self, con):
-    self.con = con
+  sense = SenseHat()
+  gyroList = []
+  pitchList = []
+  rollList = []
+  yawList = []
+
+while True:
+  gyro_only = sense.get_gyroscope()
+  gyroList.append(sense.get_gyroscope())
+  for i in gyroList:
+    pitchList.append([ i[name] for name in i.keys() if name == 'pitch' ])
+    rollList.append([ i[name] for name in i.keys() if name == 'roll' ])
+    yawList.append([ i[name] for name in i.keys() if name == 'yaw' ])
+
+
+
+
+#   print("p: {pitch}, r: {roll}, y: {yaw}".format(**gyro_only))
+
+
+
+  # def __init__(self, con):
+  #   self.con = con
