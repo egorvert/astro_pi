b5137e5333e0313e2dd3c329ced00d4132360c2c
Melon-Bowl <50581434+Melon-Bowl@users.noreply.github.com>
2021-02-15 14:34:07 +0000

    Added code for picam and light matrix

--- src/camera.py
+++ src/camera.py
@@ -1,8 +1,11 @@
 # Egor and Stepan
 from src.metric import MetricController
+from picamera import PiCamera
+import numpy as np
+from time import sleep
+import datetime
 
-
-class CameraController(MetricController):
+class CameraController(MetricController, picamera.array.PiMotionAnalysis):
   """Controller for methods and data related to the light sensor/camera.
     
   :param con: Reference to main controller
@@ -12,7 +15,24 @@ class CameraController(MetricController):
     super().__init__(1, 'camera')
     self.con = con
     self.sense = con.sense
+  def analyze(self,a): #functiont that determines whether there is motion by looking at vectors with magnitudes larger than 60
+    a = np.sqrt(
+      np.square(a['x'].astype(np.float))+
+      np.square(a['y']. astype(np.float))
+      ).clip(0,255).astype(np.uint8)
+    if (a>60).sum()>10:
+      print("Motion detected! at %s" %(datetime.datetime.now())) #change to light matrix output and save a true or false boolean to the main data file with a datetime
 
-  def measure_value(self) -> float:
-    # Reads the value from the relevent module and returns it
-    return 1
+  # Reads the value from the relevant module and returns it
+  def measure_value(self) -> float: 
+    pass
+while True: #Change this so that it doesnt get stuck
+    with picamera.PiCamera() as camera:
+      with CameraController(camera) as data_out:
+          camera.resolution = (640, 480)
+          camera.start_recording(
+                '/dev/null', format='h264', motion_output=data_out) #records for 60 s V
+          camera.wait_recording(60)
+          camera.stop_recording()
+  
+    
--- src/light_matrix.py
+++ src/light_matrix.py
@@ -1,5 +1,6 @@
 # Egor and Stepan
-
+from sense_hat import SenseHat
+from time import sleep
 
 class MatrixController():
   """Controller for methods and data related to the light matrix
@@ -9,6 +10,31 @@ class MatrixController():
   """
   def __init__(self, con):
     self.con = con
+
+  def greeting(self):
+    sense = SenseHat()
+
+    sense.set_rotation(270)
+
+    X = [255, 0, 0]  # Red
+    O = [0, 0, 0]  # Black
+
+    greeting_msg = "Hello world!"
+
+    greeting_icon = [
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, X, O, O, O, O, X, O,
+    X, O, X, O, O, X, O, X,
+    O, O, O, O, O, O, O, O,
+    O, X, O, O, O, O, X, O,
+    O, O, X, X, X, X, O, O,
+    O, O, O, O, O, O, O, O
+    ]
+
+    sense.show_message(greeting_msg, back_colour=O, text_colour=blue)
+    sleep(1)
+    sense.set_pixels(greeting_icon)
 
   def update(self, cam_changed: bool, acc_changed: bool, gyro_changed: bool):
     pass
