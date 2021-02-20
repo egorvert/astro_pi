a99aa01ca12c33371791612b3b3c41bb75f3b6a3
Gregor Maclaine <thegregormaclaine@gmail.com>
2021-02-20 02:48:48 +0000

    Added documentation for light_matrix.py

--- src/light_matrix.py
+++ src/light_matrix.py
@@ -1,4 +1,5 @@
 class FrameSequence:
+  """A namespace for frame sequences"""
   # yapf: disable
   idle = [
     ['face/smile.png', 2500],
@@ -38,7 +39,7 @@ class FrameSequence:
 
 
 class MatrixController:
-  """Controller for methods and data related to the light matrix
+  """Controller for the LED light matrix and what to show on it.
 
   :param con: Reference to main controller
   :type con: main.Controller
@@ -52,15 +53,32 @@ class MatrixController:
     self.timeperiod = 50.0
 
   def set_framerate(self, framerate: int):
+    """Sets the framerate of the main loop so that the controller
+    can calculate how long to show each frame.
+
+    :param framerate: The number ``update()`` calls per second
+    :type framerate: int
+    """
     self.timeperiod = 1000 / framerate
 
   def load_image(self, image: str):
+    """Shows a 8x8 image on the LED matrix
+
+    :param image: The image filename
+    :type image: str
+    """
     try:
       self.sense.load_image(image)
     except Exception as err:
       self.con.output.log(f'Could not show frame "{image}"', err)
 
   def update(self):
+    """Handles the iteration and dynamics of the frame sequences.
+    Should be called once per main loop cycle after the framerate has been
+    set in the ``set_framerate()`` method.\n
+    It works by either keeping the current frame, or moving onto the next frame in
+    the sequence. If no more frames are stated, it defaults to showing ``FrameSequence.idle``.
+    """
     if self.frame_time_left <= 0:
       if len(self.frame_queue) == 0:
         self.frame_queue += FrameSequence.idle
@@ -71,6 +89,16 @@ class MatrixController:
       self.frame_time_left -= self.timeperiod
 
   def start_sequence(self, cam_changed: bool, acc_changed: bool, gyro_changed: bool):
+    """Handles changing the current frame sequence in response to a possible deviance
+    found after measuring the hardware components.
+
+    :param cam_changed: Whether the camera value deviated
+    :type cam_changed: bool
+    :param acc_changed: Whether the accelerometer value deviated
+    :type acc_changed: bool
+    :param gyro_changed: Whether the gyroscope value deviated
+    :type gyro_changed: bool
+    """
     if cam_changed or acc_changed or gyro_changed:
       if cam_changed:
         self.con.output.log('Matrix: Showing camera animation')
